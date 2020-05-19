74c738648aa20e3684a6666a9ef417d3f422a5f6
Saurav M. H <sauravhiremath@gmail.com>
2020-05-19 13:42:04 +0530

    feat: single req-res added

--- src/main.go
+++ src/main.go
@@ -6,18 +6,21 @@ import (
 	"net"
 )
 
-func connect() {
-	conn, _ := net.Dial("tcp", "127.0.0.1:4000")
-
-	// Send data here
-	fmt.Fprintf(conn, "hello\n")
-	status, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		// handle error
-	}
-	fmt.Printf(status)
-}
+var initialize = "{\"requestId\": \"jb-1\", \"type\": 1, \"moduleId\": \"juno-benchmark\", \"version\": \"1.0.0\"}\n"
+var message = "{\"requestId\": \"test\", \"type\": 3, \"function\": \"tester.test\", \"arguments\": { \"req\": \"sent\" } }\n"
 
 func main() {
-	connect()
+	conn, _ := net.Dial("tcp", "127.0.0.1:4000")
+	// Send and receive initialize message here
+	fmt.Fprintf(conn, initialize)
+	initializeRes, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Printf(initializeRes)
+
+	// Send data here
+	fmt.Fprintf(conn, message)
+
+	// wait for reply
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Printf(message)
+
 }
