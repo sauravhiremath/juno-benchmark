fbbae51ae23e75518acc292467f7c89c7fbb3b0f
Saurav M. H <sauravhiremath@gmail.com>
2020-05-21 03:11:21 +0530

    feat: Parallel requests added

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module juno-benchmark
 
 go 1.14
 
-require github.com/satori/go.uuid v1.2.0
+require (
+	github.com/satori/go.uuid v1.2.0
+)
--- src/main.go
+++ src/main.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
+	"sync"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
@@ -18,7 +20,7 @@ type initMsg struct {
 	Version   string `json:"version"`
 }
 
-type funcCall struct {
+type job struct {
 	RequestID string    `json:"requestId"`
 	Type      int       `json:"type"`
 	Function  string    `json:"function"`
@@ -29,39 +31,40 @@ type arguments struct {
 	Req string `json:"req"`
 }
 
-// var initialMsg = "{\"requestId\": \"jb-1\", \"type\": 1, \"moduleId\": \"juno-benchmark\", \"version\": \"1.0.0\"}\n"
-// var message = "{\"requestId\": \"test\", \"type\": 3, \"function\": \"tester.test\", \"arguments\": { \"req\": \"sent\" } }\n"
-
-func registerModule(c *net.Conn) {
+func registerModule(c *net.Conn, sub *sync.WaitGroup) {
 	// Send module initialization request for each conn (register juno module)
 	// Create unique requestID and moduleID
 	// If not debug, dont print message, use io.Copy(ioutil.Discard) instead
+	defer sub.Done()
+
 	var i int = 0
+	uuid := uuid.NewV4().String()
 	iMsg, _ := json.Marshal(initMsg{
-		RequestID: uuid.NewV4().String(),
+		RequestID: uuid,
 		Type:      1,
-		ModuleID:  "juno-benchmark",
+		ModuleID:  "benchmark" + uuid,
 		Version:   "1.0.0",
 	})
-	fmt.Println(string(iMsg))
 	fmt.Fprintf(*c, string(iMsg)+"\n")
 	scanner := bufio.NewScanner(*c)
 	for scanner.Scan() {
-		fmt.Println("Initial message " + strconv.Itoa(i) + scanner.Text())
+		log.Println("Initial message " + strconv.Itoa(i) + scanner.Text())
 		i++
 		if i > 1 {
 			break
 		}
 	}
-	return
+	// log.Println("[INFO] Finished registering module...")
 }
 
-func callFunction(c *net.Conn) {
-	// Send callFunction payload to juno
+func callFunction(c *net.Conn, sub *sync.WaitGroup) {
+	// Send call Function request to juno
 	// Create unique requestID
-	// Parse response to check if it was successfull, (data == "200 OK")
+	// UnMarshal the response to check if it was successfull, (data == "200 OK")
 	// If not debug then do not store message use io.Copy(ioutil.Discard) instead
-	msg, _ := json.Marshal(funcCall{
+	defer sub.Done()
+
+	msg, _ := json.Marshal(job{
 		RequestID: uuid.NewV4().String(),
 		Type:      3,
 		Function:  "tester.test",
@@ -71,49 +74,68 @@ func callFunction(c *net.Conn) {
 	})
 	start := time.Now()
 	fmt.Fprintf(*c, string(msg)+"\n")
-	message, _ := bufio.NewReader(*c).ReadString('\n')
-	fmt.Println(time.Since(start))
-	fmt.Printf(message)
-	(*c).Close()
+	message, err := bufio.NewReader(*c).ReadString('\n')
+	if err != nil {
+		log.Panic("[ERROR] Message recieve failed: ", err)
+		return
+	}
+	log.Println(time.Since(start), message)
 	return
 }
 
-func spawnConn(c chan net.Conn, n int) {
+func initialize(c chan net.Conn, n int) {
+	var sub sync.WaitGroup
+
 	for i := 0; i < n; i++ {
-		conn, _ := net.Dial("tcp", "127.0.0.1:4000")
+		conn, _ := net.Dial("tcp4", "127.0.0.1:4000")
 		c <- conn
+		sub.Add(1)
+		go registerModule(&conn, &sub)
 	}
+	sub.Wait()
+}
+
+func runJobs(c *net.Conn, n int, main *sync.WaitGroup) {
+	defer main.Done()
+
+	var sub sync.WaitGroup
+	for i := 0; i < n; i++ {
+		sub.Add(1)
+		go callFunction(c, &sub)
+	}
+	sub.Wait()
+
+	// log.Println("[DEBUG] Closing connection...")
+	(*c).Close()
 }
 
 func runtimeStats() {
 	// Display info on throughput and other memStats
 }
 
-// func doWork(id int, j job) {
-// 	fmt.Printf("worker%d: started %s, working for %f seconds\n", id, j.requestID, j.throughput.Seconds())
-// }
-
-// func reqHandler(jobs <-chan job) {
-// 	conn, _ := net.Dial("tcp", "127.0.0.1:4000")
-// 	// Send and receive initialize message here
-// 	fmt.Fprintf(conn, initialMsg)
-// 	initializeRes, _ := bufio.NewReader(conn).ReadString('\n')
-// 	fmt.Printf(initializeRes)
-// }
-
 func main() {
-	// var wg sync.WaitGroup
-	conns := make(chan net.Conn, 10)
-	spawnConn(conns, cap(conns))
-	for conn := range conns {
-		fmt.Printf("In for...\n")
-		registerModule(&conn)
-		callFunction(&conn)
-		fmt.Printf("finished all...\n")
+	// ################################# CHANGE CONNECTIONS AND JOBS HERE #########################################
+	var CONN int = 100
+	var JOBS int = 100
+	// ############################################################################################################
+
+	var main sync.WaitGroup
+	conns := make(chan net.Conn, CONN)
+
+	initialize(conns, cap(conns))
+
+	for i := 0; i < cap(conns); i++ {
+		conn, more := <-conns
+		if more {
+			main.Add(1)
+			go runJobs(&conn, JOBS, &main)
+		} else {
+			log.Println("[INFO] Spawned all connections.")
+			close(conns)
+			return
+		}
 	}
-	fmt.Printf("finished all outside...\n")
-	close(conns)
-	// for conn := range conns {
-	// 	defer conn.Close()
-	// }
+	main.Wait()
+
+	log.Printf("[DEBUG] Program ended gracefully...\n")
 }
